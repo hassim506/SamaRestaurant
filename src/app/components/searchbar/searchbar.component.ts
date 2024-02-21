5c21de48499aff19a4b62a5125c531b08b468d2d
hassim506 <hassimiou061@gmail.com>
2024-02-21 13:47:31 +0100

    update

--- src/app/components/searchbar/searchbar.component.ts
+++ src/app/components/searchbar/searchbar.component.ts
@@ -5,7 +5,21 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './searchbar.component.html',
   styleUrls: ['./searchbar.component.scss'],
 })
+
 export class SearchbarComponent  implements OnInit {
+
+  transform(value: any, args?: any): any{
+    if(!value)return null;
+    if(!args) return  value;
+
+
+    args = args.toLowercase();
+
+    return value.filter((item: any ) => {
+
+      return JSON.stringify(item).toLocaleLowerCase().includes(args);
+    })
+  }
 
   constructor() { }
 
